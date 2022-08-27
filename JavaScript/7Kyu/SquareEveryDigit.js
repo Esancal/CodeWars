48d6bdef54c654d7f69c18b36fab9f0aef5ee348
Esancal <esteban.ca2000@outlook.cl>
2022-08-27 04:51:20 -0400

    Numeros a Array para trabajar digitos

--- JavaScript/7Kyu/SquareEveryDigit.js
+++ JavaScript/7Kyu/SquareEveryDigit.js
@@ -2,6 +2,13 @@
 // For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
 // Note: The function accepts an integer and returns an integer
 
-function squareDigits(num){
-  return 0;
+function squareDigits(num) {
+  arr = Array.from(String(num), Number);
+
+  let squareDigitsNum = ""; // Para que al ingresar los valores de cada digito no se sumen
+  // Sino, se consideren un String
+  for (let i = 0; i < arr.length; i++) {
+    squareDigitsNum += arr[i] * arr[i];
+  }
+  return parseInt(squareDigitsNum); // parseInt para transformar el String a numero
 }
