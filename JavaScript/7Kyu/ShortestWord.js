31b12fd889fd155edcea378e4ab0c3cf4c565351
Esancal <esteban.ca2000@outlook.cl>
2022-08-29 23:19:19 -0400

    Recorrido de un Array de Arrays

--- JavaScript/7Kyu/ShortestWord.js
+++ JavaScript/7Kyu/ShortestWord.js
@@ -2,5 +2,20 @@
 // String will never be empty and you do not need to account for different data types.
 
 function findShort(s) {
-  
+  let arr = s.split(" "); // Creo un Array de Strings
+
+  let count = arr[0].length; // Seteo un valor mas bajo para la cuenta
+
+  for (let i = 0; i < arr.length; i++) {
+    // Repito el ciclo por el largo del array
+    if (arr[i].length < count) {
+      // Si el largo del array es menor al largo del seteo base:
+      count = arr[i].length; // Se reemplaza el valor de "count" con el nuevo valor mas bajo al contar el largo del String
+    }
+  }
+  return count;
 }
+
+// function findShort(s){ // Este lo obtuve como la respuesta de otras personas
+//   return Math.min.apply(null, s.split(' ').map(w => w.length));
+// }
